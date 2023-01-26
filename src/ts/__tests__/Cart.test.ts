d5d8795c268b266e09129e234ab27f925d9ea67d
Dima Shirkov <dimashirkov@list.ru>
2023-01-26 11:59:04 +0300

    refactoring the delElement method and tests

--- src/ts/__tests__/Cart.test.ts
+++ src/ts/__tests__/Cart.test.ts
@@ -3,48 +3,35 @@ import Book from '../domain/Book';
 import MusicAlbum from '../domain/MusicAlbum';
 import Movie from '../domain/Movie';
 
-test('new card should be empty', () => {
-  const cart = new Cart();
+const cart = new Cart();
 
+const warAndPiece = new Book(1001, 'War and Piece', 'Leo Tolstoy', 2000, 1225);
+const meteora = new MusicAlbum(1008, 'Meteora', 'Linkin Park', 900);
+const theAvengers = new Movie('the Avengers', 2012, 'США', 'Avengers Assemble!', 'фантастика', 137, 370, 1010);
+
+
+test('new card should be empty', () => {
   expect(cart.items.length).toBe(0);
 });
 
 test('there must be three items in the cart', () => {
-  const cart = new Cart();
-  cart.add(new Book(1001, 'War and Piece', 'Leo Tolstoy', 2000, 1225));
-  cart.add(new MusicAlbum(1008, 'Meteora', 'Linkin Park', 900));
-  cart.add(new Movie('the Avengers', 2012, 'США', 'Avengers Assemble!', 'фантастика', 137, 370, 1010))
+  cart.add(warAndPiece);
+  cart.add(meteora);
+  cart.add(theAvengers);
 
   expect(cart.items.length).toBe(3);
 });
 
 test('get purchase amount', () => {
-  const cart = new Cart();
-
-  cart.add(new Book(1001, 'War and Piece', 'Leo Tolstoy', 2000, 1225));
-  cart.add(new MusicAlbum(1008, 'Meteora', 'Linkin Park', 900));
-  cart.add(new Movie('the Avengers', 2012, 'США', 'Avengers Assemble!', 'фантастика', 137, 370, 1010))
-
-  expect(cart.withoutDiscounts()).toBe(3270)
+  expect(cart.getAmountWithoutDiscounts()).toBe(3270)
 })
 
 test('receive discounted purchase amount', () => {
-  const cart = new Cart();
-
-  cart.add(new Book(1001, 'War and Piece', 'Leo Tolstoy', 2000, 1225));
-  cart.add(new MusicAlbum(1008, 'Meteora', 'Linkin Park', 900));
-  cart.add(new Movie('the Avengers', 2012, 'США', 'Avengers Assemble!', 'фантастика', 137, 370, 1010))
-
-  expect(cart.withDiscount(20)).toBe(2616)
+  expect(cart.getDiscountedAmount(20)).toBe(2616)
 })
 
 test('removing an item from the cart', () => {
-  const cart = new Cart();
-
-  cart.add(new Book(1001, 'War and Piece', 'Leo Tolstoy', 2000, 1225));
-  cart.add(new MusicAlbum(1008, 'Meteora', 'Linkin Park', 900));
-  cart.add(new Movie('the Avengers', 2012, 'США', 'Avengers Assemble!', 'фантастика', 137, 370, 1010))
   cart.delElement(1008)
 
-  expect(cart.items.length).toBe(3)
+  expect(cart.items.length).toBe(2)
 })
--- src/ts/service/Cart.ts
+++ src/ts/service/Cart.ts
@@ -1,35 +1,36 @@
 import Buyable from '../domain/Buyable';
 
 export default class Cart {
-    private _items: Buyable[] = [];
+    private _items: Buyable[] = []; 
 
     add(item: Buyable): void {
-        this._items.push(item);
+      this._items.push(item);
     }
 
     get items(): Buyable[] {
-        return [...this._items]; 
+      return [...this._items]; 
     }
 
-    withoutDiscounts(): number {
-        let amount = [];
-        
-        for(let item in this.items){
-          amount.push(this.items[item].price);
-        }
-        
-        return amount.reduce((sum, current) => sum + current, 0)
+    getAmountWithoutDiscounts(): number {
+      let amount = [];
+      
+      for(let item in this.items){
+        amount.push(this.items[item].price);
       }
-    
-      withDiscount(perc: number): number {
-        return this.withoutDiscounts() - this.withoutDiscounts() * perc / 100;
-      }
+      
+      return amount.reduce((sum, current) => sum + current, 0)
+    }
   
-      delElement(index: number) {
-        Object.entries(this._items).forEach(([key, value]: any) => {
-            if(value.id === index){
-            delete this._items[key];
-          }
-        })
-      }  
+    getDiscountedAmount (perc: number): number {
+      let purchaseAmount = this.getAmountWithoutDiscounts();
+      return purchaseAmount -= purchaseAmount * perc / 100;
+    }
+
+    delElement(index: number): any {
+      return this._items = this._items.filter(item => item.id != index)
+    }   
 }
+
+
+// this.items - это же объект, не понимаю почему на него расспространяются методы для работы с массивом
+// по именам в функции следовал данному гайду https://habr.com/ru/post/558902/
